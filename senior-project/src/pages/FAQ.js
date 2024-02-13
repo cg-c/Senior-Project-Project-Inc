1b2af4040d9a39eec87ecd15e08def5dfec14e9c
cg-c <52759655+cg-c@users.noreply.github.com>
2024-02-13 15:53:56 -0500

    Routes Basic Setup

---
+++ senior-project/src/pages/FAQ.js
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+
+export default function FAQ() {
+    return (
+        <>
+            <Header />
+            <h2>FAQ Page</h2>
+        </>
+    )
+}
