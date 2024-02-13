8a131380e19bdb6741b729b5b1c2ac9b7f63faf1
cg-c <52759655+cg-c@users.noreply.github.com>
2024-02-13 16:31:43 -0500

    More Pages

--- senior-project/src/App.js
+++ senior-project/src/App.js
@@ -2,7 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import FAQ from './pages/FAQ';
+import NoPage from './pages/NoPage';
 
+import ProjPitch from './pages/ProjPitch';
+import AdvProj from './pages/AdvProj';
+import MyTeam from './pages/MyTeam';
+import AdvStudents from './pages/AdvStudents';
+import Grades from './pages/Grades';
 
 
 function App() {
@@ -10,10 +16,21 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
+          /* Info Pages */
           <Route index element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/FAQ" element={<FAQ />} />
+
+          /* Need to be logged in */
+          <Route path="/projectpitch" element={<ProjPitch />} />
+          <Route path="/advisorsprojects" element={<AdvProj />} />
+          <Route path="/myteam" element={<MyTeam />} />
+          <Route path="/students" element={<AdvStudents />} />
+          <Route path="/grades" element={<Grades />} />
+
+          /* Error Page */
+          <Route path="*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>
     </div>
