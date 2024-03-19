6449730b461da5643b844181d045c38a921159b7
NickHobin <73544884+NickHobin@users.noreply.github.com>
2024-03-19 15:29:10 -0400

    added intro video

--- senior-project/src/components/HeroSection.js
+++ senior-project/src/components/HeroSection.js
@@ -12,7 +12,7 @@ function HeroSection() {
       <h1 className='rounded-md bg-black/15 px-4 py-2 text-5xl'>SENIOR PROJECT</h1>
 
       <div className='py-8'>
-        <a href="https://youtube.com" target="_blank">
+        <a href="https://mediasite.video.ufl.edu/Mediasite/Play/8ebcb534f140433296041170863ba8631d" target="_blank">
         <button className='rounded-md bg-black/40 px-2 py-2 text-md text-white hover:bg-black/60'> WATCH INTRO</button>
         </a>      
       </div>
