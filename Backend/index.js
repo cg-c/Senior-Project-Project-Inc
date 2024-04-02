e15d32691be044cbbff796b4abe5a8cf1db89ee5
maingotj <maingotj@ufl.edu>
2024-04-02 13:50:42 -0400

    wallet added

--- Backend/index.js
+++ Backend/index.js
@@ -7,11 +7,28 @@ oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
 app.use(bodyParser.json());
 
+let clientOpts = {};
+if (process.platform === 'win32') {
+  // Windows
+  // If you use backslashes in the libDir string, you will
+  // need to double them.
+  clientOpts = { libDir: '..\\instantclient_21_13' };
+} else if (process.platform === 'darwin' && process.arch === 'x64') {
+  // macOS Intel
+  clientOpts = { libDir: '../instantclient_21_13' };
+}
+// else on other platforms like Linux the system library search path MUST always be
+// set before Node.js is started, for example with ldconfig or LD_LIBRARY_PATH.
+
+// enable node-oracledb Thick mode
+oracledb.initOracleClient(clientOpts);
+
 const dbConfig = {
   user: "admin",
   password: "CapeTownRox28!",
   connectString:
     "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.us-ashburn-1.oraclecloud.com))(connect_data=(service_name=g919c578ac880c3_j1r684plpz959lmg_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))",
+  connectString: "j1r684plpz959lmg_high",
 };
 
 const data = [
